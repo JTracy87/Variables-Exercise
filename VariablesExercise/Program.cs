65d3d8e3186f22fb6485ea957811aac335a4f112
Joseph Tracy <JTracy87@gmail.com>
2023-01-16 18:31:15 -0800

    finished VariablesExercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string customerName = "Joseph";
+
+            int disneylandTrips = 4;
+
+            char tripScore = 'A';
+
+            bool isSatisfied = true;
+
+            double parkHop = 2;
+
+            decimal moneySpent = 725.25m;
+
+            Console.WriteLine($"Hello {customerName}, you have been to Disneyland {disneylandTrips} times.");
+            Console.WriteLine($"You have given your trip a score of {tripScore}.");
+            Console.WriteLine($"When asked if you were satisfied with your trip, you responded with {isSatisfied}.");
+            Console.WriteLine($"On your last trip, you park hopped {parkHop} times and spent ${moneySpent}.");
         }
     }
 }
